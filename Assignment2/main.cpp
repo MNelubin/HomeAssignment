704c4adad7c1c38fcff9a97a8ba7bf5cd143ab46
MaxNelubin <max-nelubin@yandex.ru>
2024-10-04 03:21:04 +0300

    working

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -7,28 +7,28 @@
 
 int main()
 {
-    int *d;
-    std::ofstream inf;
-    inf.open("test1.bin",std::ios::binary|std::ios::in);
-    std::ifstream outf;
+   
+    std::ofstream outf;
     outf.open("test1.bin",std::ios::binary|std::ios::out);
+    std::ifstream inf;
+    inf.open("test1.bin",std::ios::binary|std::ios::in);
     int sz = std::filesystem::file_size("test1.bin");
     std::cout<<sz<<std::endl;
     char* buffer = new char [sz];
-    
-    outf.read((char*)&buffer,sizeof(buffer));
+    //int buffer = 12345;
+    inf.read(buffer,sizeof(buffer));
     
     for (int i = 0; i < sz; ++i)
     {
     	std::cout<<buffer[i];
     }
     std::cout<<std::endl;
-    //inf.write((char*)&buffer,sizeof(buffer));
+    //outf.write((char*)&buffer,sizeof(buffer));
 
     inf.close();
     outf.close();
 	std::cout<<buffer<<std::endl;
 
-    delete [] d;
+    delete [] buffer;
     return 0;
 }
