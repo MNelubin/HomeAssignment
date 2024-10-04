b17ff4e202a9e3d3a1bcc4b64bc1a98ec76f79e2
MaxNelubin <max-nelubin@yandex.ru>
2024-10-04 03:35:25 +0300

    fixed bug(self-deleting file)

--- Assignment2/main.cpp
+++ Assignment2/main.cpp
@@ -8,22 +8,22 @@
 int main()
 {
    
-    std::ofstream outf;
-    outf.open("test1.bin",std::ios::binary|std::ios::out);
     std::ifstream inf;
     inf.open("test1.bin",std::ios::binary|std::ios::in);
     int sz = std::filesystem::file_size("test1.bin");
     std::cout<<sz<<std::endl;
     char* buffer = new char [sz];
     //int buffer = 12345;
-    inf.read(buffer,sizeof(buffer));
+    inf.read(buffer,sz);
     
     for (int i = 0; i < sz; ++i)
     {
     	std::cout<<buffer[i];
     }
     std::cout<<std::endl;
-    //outf.write((char*)&buffer,sizeof(buffer));
+    std::ofstream outf;
+    outf.open("test1.bin",std::ios::binary|std::ios::out);
+    outf.write(buffer,sz);
 
     inf.close();
     outf.close();
