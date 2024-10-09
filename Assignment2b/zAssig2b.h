c6a0203246cf964724be52d5f30caa6472885bc3
MaxNelubin <max-nelubin@yandex.ru>
2024-10-10 00:24:24 +0300

    upgrading polish(adding support of dot,+ making other files

---
+++ Assignment2b/zAssig2b.h
@@ -0,0 +1,20 @@
+/* Maxim Nelyubin st132907@student.spbu.ru
+	Assighment 2b
+*/
+#ifndef ZAG_H
+#define ZAG_H
+
+
+
+#include <iostream>
+#include <map>
+#include <string>
+
+namespace problem_2b
+{
+	std::string r_polish();
+}
+
+
+
+#endif
